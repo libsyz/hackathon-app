080a3ea6d96d1c83b3ff681ec3bc9eb313f6d74b
miguel jimenez <denkatalabeno@gmail.com>
2018-10-13 15:02:00 +0200

    implemented the basics of the countdown clock!

--- src/components/countdown/countdown.ts
+++ src/components/countdown/countdown.ts
@@ -11,12 +11,52 @@ import { Component } from '@angular/core';
   templateUrl: 'countdown.html'
 })
 export class CountdownComponent {
-
+  countDownActive: boolean = false;
   text: string;
+  time: number;
+  minutes: any = 15;
+  seconds: any = "00";
+  info: string = "START";
+  fifteenMinutes: number = 900;
 
   constructor() {
     console.log('Hello CountdownComponent Component');
     this.text = 'Hello World';
   }
 
+  startCountDown(){
+    if (this.countDownActive == false) {
+      let timeNow = Date.now();
+      let then = timeNow + this.fifteenMinutes * 1000;
+      this.time = then - timeNow;
+      this.info = "GO!"; 
+      this.updateTimer()
+      this.countDownActive = true;    
+    }
+  }
+
+  updateTimer() {
+    setInterval(()=> {
+      this.time = this.time - 1000;
+      this.minutes = this.getMinutes(this.time)
+      this.seconds = this.getSeconds(this.time) 
+    }, 1000)
+    
+  }
+
+  getMinutes(time) {
+    let minutes: any = Math.floor(time/1000/60);
+    if (minutes < 10 ) {
+      minutes = `0${minutes}`
+      }
+    return minutes;
+    }
+
+  getSeconds(time) {
+    let seconds: any = this.time/1000%60;
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    return seconds;
+  }
 }
