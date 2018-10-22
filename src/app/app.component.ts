eb1c4a2fc1b85847da9ecf10d3656f740f862219
miguel jimenez <denkatalabeno@gmail.com>
2018-10-22 15:05:45 +0200

    added html structure for define-test-protocol page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
+import { HomePage } from './../pages/home/home';
+import { DefineTestProtocolPage } from './../pages/define-test-protocol/define-test-protocol';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = DefineTestProtocolPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
